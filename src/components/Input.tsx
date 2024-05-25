5222c3a72e129de0744c540e447b61dcb59d274e
Stephen McLean <stephenmclean@Stephens-MacBook-Pro.local>
2024-05-25 10:00:04 +0100

    add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,38 @@
+import classNames from "classnames";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: boolean;
+  fullWidth?: boolean;
+}
+
+export const Input = ({
+  label,
+  error,
+  fullWidth,
+  ...inputProps
+}: InputProps) => {
+  return (
+    <div
+      className={classNames("flex flex-col gap-1 font-sans", {
+        "w-full": fullWidth,
+      })}
+    >
+      {label && (
+        <label className="text-gray-600 font-medium text-sm">{label}</label>
+      )}
+      <input
+        {...inputProps}
+        className={classNames(
+          "bg-gray-50 text-gray-500 focus:outline-none  rounded px-2 py-1 transition-colors placeholder:text-gray-300",
+          {
+            "border border-gray-100 focus:border-gray-300 hover:border-gray-300":
+              !error,
+            "border border-error-500": error,
+            "w-full": fullWidth,
+          }
+        )}
+      />
+    </div>
+  );
+};
