fa74e0153f3a080b96ca02017e30eb5864d63a41
Stephen McLean <stephenmclean@Stephens-MacBook-Pro.local>
2024-05-25 08:59:32 +0100

    configure tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,43 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#EDF5F9",
+          100: "#CAE1EE",
+          500: "#86BBD8",
+        },
+        accent: {
+          50: "#FCEBF6",
+          100: "#F6C2E3",
+          500: "#6A0F49",
+        },
+        error: {
+          50: "#FDEAEE",
+          100: "#F9BFCB",
+          500: "#EE4266",
+        },
+      },
+    },
     fontFamily: {
       sans: ["Inter", "sans-serif"],
+    },
+    colors: {
+      white: "#FDFFFC",
+      gray: {
+        50: "#F3F3F3",
+        100: "#DCDCDC",
+        200: "#C5C5C5",
+        300: "#AEAEAE",
+        400: "#979797",
+        500: "#808080",
+        600: "#686868",
+        700: "#515151",
+        800: "#3A3A3A",
+        900: "#232323",
+        950: "#0C0C0C",
+      },
     },
   },
   plugins: [],
